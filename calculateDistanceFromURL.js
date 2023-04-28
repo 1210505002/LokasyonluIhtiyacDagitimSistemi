a5b5c147cbc764efb999076391546d3f3e3711b0
Reyhan Türkkal <94196332+1210505002@users.noreply.github.com>
2023-04-29 01:19:54 +0300

    Update calculateDistanceFromURL.js

--- calculateDistanceFromURL.js
+++ calculateDistanceFromURL.js
@@ -1,16 +1,21 @@
 // OpenStreetMap OSRM'i kullanarak iki nokta arasındaki rota üzerindeki mesafeyi URL kullanarak hesaplamandi
-function calculateDistanceFromURL(startLat, startLon, endLat, endLon) {
-    const url = `https://router.project-osrm.org/route/v1/driving/${startLon},${startLat};${endLon},${endLat}?overview=false&steps=false
-    `;
-    return new Promise((resolve, reject) => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          let distanceInMeters = data.routes[0].distance;
-          let steps = data.routes[0].legs[0].steps;
-          resolve({ distanceInMeters, steps });
-        })
-        .catch((error) => reject(error));
-    });
+const cache = new Map(); // Önbellek
+
+async function calculateDistanceFromURL(startLat, startLon, endLat, endLon) {
+  const cacheKey = `${startLat},${startLon},${endLat},${endLon}`; // Önbellek anahtarı
+  if (cache.has(cacheKey)) {
+    return cache.get(cacheKey); // Önbellekte veri varsa, önbellekten döndür
   }
-  export {calculateDistanceFromURL}
+
+  const url = `http://router.project-osrm.org/route/v1/driving/${startLon},${startLat};${endLon},${endLat}?overview=false&steps=false`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const distanceInMeters = data.routes[0].distance;
+  const steps = data.routes[0].legs[0].steps;
+  const result = { distanceInMeters, steps };
+
+  cache.set(cacheKey, result); // Önbelleğe ekle
+
+  return result;
+}
+
